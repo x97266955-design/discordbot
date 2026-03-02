59c65ef26c79bbc6814ee2510806158c1aa88f99
x97266955-design <x97266955@gmail.com>
2026-03-02 21:47:15 +0900

    Add Express server to index.js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const { Client, GatewayIntentBits } = require('discord.js');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bot is running!');
+});
+
+app.listen(PORT, () => {
+  console.log('Web server is running.');
+});
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
