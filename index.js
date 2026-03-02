5a18feb56c2896498996805afc7876d7eae1e77f
x97266955-design <x97266955@gmail.com>
2026-03-02 21:23:40 +0900

    Initialize Discord bot with basic setup

---
+++ index.js
@@ -0,0 +1,11 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once('ready', () => {
+  console.log('Bot is ready');
+});
+
+client.login(process.env.DISCORD_TOKEN);
